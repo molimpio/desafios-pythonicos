cc6f13b0828ee7d0e9362a7863edbe5ce48044fb
Moises <moiza.olimpio@gmail.com>
2022-10-11 15:59:28 -0300

    Challenge 03 refactor using join

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -15,7 +15,8 @@ Assuma que a e b tem tamanho 2 ou maior.
 
 def mix_up(a, b):
     # +++ SUA SOLUÇÃO +++
-    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
+
+    return ''.join([b[:2], a[2:], ' ', a[:2], b[2:]])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
